1b1d4df8164d4b6d8be5e362be0a58ab8a6275d0
another-coder-jvs <jvs.thedev@gmail.com>
2026-08-05 06:00:57 +0000

    added drive permission

--- steps_to_setup.sh
+++ steps_to_setup.sh
@@ -1,8 +1,13 @@
 echo "Installing Dependancies"
 pip install -r requirements.txt
 
+
 echo "Installing Ngrok"
 pip -q install fastapi uvicorn pyngrok nest_asyncio
 cd backend
 
 echo "Now First start NGROK with start_ngrok.py ! \nthen Backend with python main.py"
+
+echo "Need drive permission to store the masks !" 
+from google.colab import drive
+drive.mount('/content/drive')
