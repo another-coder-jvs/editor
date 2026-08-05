cf6d23acdecb5e0013e2eca8cc6b7e2beaa5adb1
another-coder-jvs <jvs.thedev@gmail.com>
2026-08-05 05:31:48 +0000

    added steps

---
+++ start_ngrok.py
@@ -0,0 +1,12 @@
+from pyngrok import ngrok
+ngrok.set_auth_token("3HRjpFMqfHffIlX0oPqHITBQalK_457XLfBzcPqizqijAyV1p")
+
+ngrok.kill()
+tunnel2 = ngrok.connect(
+    8000,
+    proto="http"
+)
+
+print(tunnel2.public_url)
+print(tunnel2.public_url+"/docs")
+
---
+++ steps_to_setup.sh
@@ -0,0 +1,8 @@
+echo "Installing Dependancies"
+pip install -r requirements.txt
+
+echo "Installing Ngrok"
+pip -q install fastapi uvicorn pyngrok nest_asyncio
+cd backend
+
+echo "Now First start NGROK with start_ngrok.py ! \nthen Backend with python main.py"
