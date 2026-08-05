a21b150f4ac39ba5f2e74d2854ad425aafa3a3ce
another-coder-jvs <jvs.thedev@gmail.com>
2026-08-05 07:17:12 +0000

    updated- 05/08/2026 07:17:11

--- backend/main.py
+++ backend/main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "https://cover-ended-growing.ngrok-free.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
