93daa5de2e93dea9c343bb037321957498709410
another-coder-jvs <jvs.thedev@gmail.com>
2026-08-05 06:41:14 +0000

    updated- 05/08/2026 06:41:14

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 OUTPUT_DIR = Path("../outputs")
-TEMP_DIR = Path("../drive/MyDrive/project_folders/temp")
+TEMP_DIR = Path("/content/drive/MyDrive/project_folders/temp") #Path("../drive/MyDrive/project_folders/temp")
 PROJECTS_DIR = Path("../projects")
 WEIGHT_DIR = Path("../weights")
